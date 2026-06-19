eb14f86681f5becd780a9f5fb296d1c1a5a9b053
Adam Pelc <sikulin94@gmail.com>
2026-06-19 18:12:03 +0200

    Add types package and define interfaces for primitive values

--- slices/slices.go
+++ slices/slices.go
@@ -1,14 +1,9 @@
 package slices
 
 import (
+	"github.com/adagit94/gotils/types"
 	"slices"
 )
-
-type Int interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
-}
-
 
 func Map[S ~[]E, E any, EE any](s S, f func(e E, i int) EE) []EE {
 	ss := make([]EE, len(s))
@@ -44,17 +39,17 @@ func Flat[S ~[][]E, E any](s S) []E {
 	return ss
 }
 
-func ExpandRange[R [2]N, N Int](r R) []N {
-	rr := make([]N, 0, r[1] - r[0] + 1)
-	
+func ExpandRange[R [2]N, N types.Int](r R) []N {
+	rr := make([]N, 0, r[1]-r[0]+1)
+
 	for n := r[0]; n <= r[1]; n++ {
 		rr = append(rr, n)
 	}
-	
+
 	return rr
 }
 
-func ExpandRanges[Rs []R, R [2]N, N Int](rs Rs) [][]N {
+func ExpandRanges[Rs []R, R [2]N, N types.Int](rs Rs) [][]N {
 	rrs := make([][]N, 0, len(rs))
 
 	for _, r := range rs {
@@ -64,6 +59,6 @@ func ExpandRanges[Rs []R, R [2]N, N Int](rs Rs) [][]N {
 	return rrs
 }
 
-func ExpandRangesFlat[Rs []R, R [2]N, N Int](rs Rs) []N {
+func ExpandRangesFlat[Rs []R, R [2]N, N types.Int](rs Rs) []N {
 	return Flat(ExpandRanges(rs))
 }
